2511f1a7a48be43bbe0c4ac622a901858be6a960
Daniel Hrdinka <dan.hrdinkatu@gmail.com>
2025-08-30 18:19:59 +0200

    PrvniVerzovani

---
+++ DU5_Hrdinka.py
@@ -0,0 +1,31 @@
+# Řešení úloh DU5 (Hrdinka)
+
+numbers = [1, 2, 4, -6, 7, 8, 100, -125, 11, 123]
+names = ["Petr", "Ales", "Honza", "Lenka", "Andrea", "Alice"]
+random_codes = ["1-okdsaaa", "0-nFnldd", "0-AA", "0-uwqqq", "2-ZSTh", "0-RKOcsxxx", "1-LwWtss", "0-cdKiddd", "2-KpAAaa", "3-sOdSxhcds"]
+
+# 1. pomocí cyklu for vypište čísla ze seznamu numbers, ale přeskočte záporná čísla.
+for number in numbers:
+    if number < 0:
+        continue
+    print(number)
+
+# 2. pomocí cyklu while vypište všechna jména, pokud narazíš na jméno 'Alice' cyklus ukonči
+i = 0
+while True:
+    if names[i] == "Alice" or i >= len(names):
+        break
+    print(names[i])
+    i += 1
+
+# 3. pomocí list comprehension vytvoř nový list, který bude obsahovat pouze prvky z 'random_codes', které obsahují 0
+random_code_0 = [rc for rc in random_codes if "0" in rc]
+print(random_code_0)
+
+# Dobrovolny ukol pro pokrocile (nebude bodovan): Vypis všechny pod-seznamy s alespoň třemi po sobě jdoucími stejnými znaky v seznamu 'random_codes'
+random_code_3 = []
+for triple in random_codes:
+    for i in range(len(triple)-2):
+        if triple.upper()[i] == triple.upper()[i+1] == triple.upper()[i+2]:
+            random_code_3.append(triple)
+print(random_code_3)
